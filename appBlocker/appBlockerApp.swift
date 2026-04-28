b20f38fbf41d0597008d04edde809434d040ab59
RoisinPin <roisinpinches@gmail.com>
2026-04-28 16:27:33 +0100

    Start for appBlocker intended to curb doomscrolling

---
+++ appBlocker/appBlockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  appBlockerApp.swift
+//  appBlocker
+//
+//  Created by Roisin Pinches on 28/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct appBlockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
